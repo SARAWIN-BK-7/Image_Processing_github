c807c0700877f1032545465ce86c85fe09f10df3
SARAWIN_B <sarawin.14156@gmail.com>
2022-09-08 21:56:37 +0700

    first commit

---
+++ Eages_detec.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+image = cv2.imread('test_room\Ping1.jpg')
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+hgt, wdt,_ = image.shape
+# Sobel Edges
+x_sobel = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
+y_sobel = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+plt.figure(figsize=(20, 20))
+plt.subplot(3, 2, 1)
+plt.title("Original")
+plt.imshow(image)
+plt.subplot(3, 2, 2)
+plt.title("Sobel X")
+plt.imshow(x_sobel)
+plt.subplot(3, 2, 3)
+plt.title("Sobel Y")
+plt.imshow(y_sobel)
+sobel_or = cv2.bitwise_or(x_sobel, y_sobel)
+plt.subplot(3, 2, 4)
+plt.imshow(sobel_or)
+laplacian = cv2.Laplacian(image, cv2.CV_64F)
+plt.subplot(3, 2, 5)
+plt.title("Laplacian")
+plt.imshow(laplacian)
+## There are two values: threshold1 and threshold2.
+## Those gradients that are greater than threshold2 => considered as an edge
+## Those gradients that are below threshold1 => considered not to be an edge.
+## Those gradients Values that are in between threshold1 and threshold2 => either classiﬁed as edges or non-edges
+# The first threshold gradient
+canny = cv2.Canny(image, 50, 120)
+plt.subplot(3, 2, 6)
+plt.imshow(canny)
---
+++ test.py
@@ -0,0 +1,26 @@
+import cv2 # import library OpenCV 
+import numpy as np # import library Numpy 
+
+
+img = cv2.imread("test_room\cat2.png") # Read the image file
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert the gray
+
+half_gray = img_gray[0:600, 0:600] # Set img_gray size
+half_color = img[0:600, 600:1200] # Set img size
+
+
+gray3channel = np.dstack((half_gray, half_gray, half_gray)) # Sort Array 3 channel
+new_img = np.hstack((gray3channel, half_color)) # build a new_img by gray3channel and half_color come together
+
+
+
+# ---------------------------- Display image --------------------------------------
+
+print(img.shape) # print img.shape, #TODO: check image size(600,1200,3)
+
+# * cv2.imshow('Grayscale image',half_gray) 
+# * v2.imshow('test',half_color )
+cv2.imshow('cat_twotone', new_img) # display the new_img 
+# * cv2.imshow('gray',img_gray)
+cv2.waitKey() # waitKey is called when the image is finished
+# * cv2.imwrite('filename.png',new_img)
