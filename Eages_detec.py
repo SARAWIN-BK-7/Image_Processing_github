029b57a8ad1ae7ce5c11b50f8ded91bd9e300416
SARAWIN_B <sarawin.14156@gmail.com>
2022-09-10 19:16:14 +0700

    Feature_detection

--- Eages_detec.py
+++ Eages_detec.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 image = cv2.imread('test_room\Ping1.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-hgt, wdt,_ = image.shape
+hgt, wdt, _ = image.shape
 # Sobel Edges
 x_sobel = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
 y_sobel = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
@@ -26,10 +26,10 @@ laplacian = cv2.Laplacian(image, cv2.CV_64F)
 plt.subplot(3, 2, 5)
 plt.title("Laplacian")
 plt.imshow(laplacian)
-## There are two values: threshold1 and threshold2.
-## Those gradients that are greater than threshold2 => considered as an edge
-## Those gradients that are below threshold1 => considered not to be an edge.
-## Those gradients Values that are in between threshold1 and threshold2 => either classiﬁed as edges or non-edges
+# There are two values: threshold1 and threshold2.
+# Those gradients that are greater than threshold2 => considered as an edge
+# Those gradients that are below threshold1 => considered not to be an edge.
+# Those gradients Values that are in between threshold1 and threshold2 => either classiﬁed as edges or non-edges
 # The first threshold gradient
 canny = cv2.Canny(image, 50, 120)
 plt.subplot(3, 2, 6)
