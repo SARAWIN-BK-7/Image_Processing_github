8d78ace8cce8ffdf835d856afcc55181ffa8aa2d
SARAWIN_B <sarawin.14156@gmail.com>
2022-10-17 10:25:49 +0700

    add jupyter and test matplotlib for midterm_exam

--- mid_Sarawin.py
+++ mid_Sarawin.py
@@ -1,13 +1,20 @@
 import numpy as np 
 import cv2 
 
-
+# NOTE -  Read the image file
 origi_img = cv2.imread("medicine_with_noise_mid.jpg")
+
+# NOTE - Convert the original image to grayscale  
 img_gray = cv2.cvtColor(origi_img, cv2.COLOR_BGR2GRAY)
 
-# kernel = np.ones((5,5),np.float32)
-dst = blur = cv2.GaussianBlur(img_gray,(5,5),0)
+# NOTE - Create kernel array or matrix 5*5 by all members = 1
+kernel = np.ones((5,5),np.float32) 
 
+                     
 
-cv2.imshow('Photo_midterm',dst)
+# NOTE - test GaussianBlur by opencv
+# dst = blur = cv2.GaussianBlur(origi_img,(5,5),0)
+
+# NOTE -  Display image output
+cv2.imshow('Photo_midterm',origi_img)
 cv2.waitKey()
